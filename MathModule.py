a540a352ff25a298078f5810c0876a1a4b1ae2eb
Star24680 <gulerumaan1@gmail.com>
2024-10-28 17:43:45 +0500

    CoMmIt

---
+++ MathModule.py
@@ -0,0 +1,7 @@
+import math
+print("The floor and celing value of 23.56 are: "+str(math.floor(23.56))+" "+str(math.ceil(23.56)))
+x=10
+y=-15
+print("Value of x after copying sing from Y is"+ str(math.copysign(x,y)))
+print("Absolute value of -96 and 56 are"+ str(math.fabs(-96))+str(math.fabs(56)))
+print("The GCD of 24 and 56 is "+ str(math.gcd(24,56)))
